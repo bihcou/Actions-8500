31c7cc8e1c811f17c53fbcdc1ab6c6ae971d4118
bihcou <59480384+bihcou@users.noreply.github.com>
2020-12-01 20:28:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -5,8 +5,11 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Uncomment a feed source
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomato package/theme
 git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly-dark package/theme-dark
 git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly package/theme1
